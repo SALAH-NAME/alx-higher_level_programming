6eb374fbcc503d5d8381d78fbd5af38172656b52
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-09 13:17:11 +0000

    fix 1->4

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if number < 0:
-	last = -last
+    last = -last
 
 if last > 5:
-	status = "and is greater than 5"
+    status = "and is greater than 5"
 elif last == 0:
-	status = "and is 0"
+    status = "and is 0"
 else:
-	status = "and is less than 6 and not 0"
+    status = "and is less than 6 and not 0"
 
 print(f"Last digit of {number} is {last} {status}")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alf in range(97,123):
-	if alf == ord('q') or alf == ord('e'):
-		continue
-	print("{}".format(chr(alf)), end='')
+for alf in range(97, 123):
+    if alf == ord('q') or alf == ord('e'):
+        continue
+    print("{}".format(chr(alf)), end='')
