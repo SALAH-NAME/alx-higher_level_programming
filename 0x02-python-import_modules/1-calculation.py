6ddec2a66d800b910478cb175b16edebf139f3a6
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-11 16:47:50 +0000

    fix 1-cal

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import calculator_1 as cal
     a, b = 10, 5
-    print(f"{a} + {b} = {cal.add(a,b)}")
-    print(f"{a} - {b} = {cal.sub(a,b)}")
-    print(f"{a} * {b} = {cal.mul(a,b)}")
-    print(f"{a} / {b} = {cal.div(a,b)}")
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
