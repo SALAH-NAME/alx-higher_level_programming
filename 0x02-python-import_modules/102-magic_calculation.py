12356d6240650d049b251c69f4eafc0a8e730ca0
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-11 18:58:11 +0000

    102-magic new

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,8 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import sub, add
-
     if a < b:
         c = add(a, b)
-        for i in range(4):
+        for i in range(4, 6):
             c = add(c, i)
             return (c)
     else:
