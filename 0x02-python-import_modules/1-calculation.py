7eb9cb41da7b166ef003713bb4c3bcfee017ebfd
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-11 16:48:42 +0000

    fix 1-cal again

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import calculator_1 as cal
-    a, b = 10, 5
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, cal.add(a, b)))
     print("{} - {} = {}".format(a, b, cal.sub(a, b)))
     print("{} * {} = {}".format(a, b, cal.mul(a, b)))
