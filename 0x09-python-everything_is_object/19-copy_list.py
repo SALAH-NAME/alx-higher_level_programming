86559ecd0b1948708c0ebb923368b0198e63aee0
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-04 17:44:25 +0000

    19 fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def copy_list(tab):
-    """
-    Returns a copy of a list
 
-    Args:
-        tab (list): The input list to be copied
-    """
+def copy_list(tab):
     return tab[:]
