00d3fab1740e8df2365c14c451a943be0e7f9ccd
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-04 17:38:29 +0000

    new

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def copy_list(l):
+    """
+    This function takes a list as an argument and returns a shallow copy of the list.
+    :param l: list
+    :return: list
+    """
     return l[:]
