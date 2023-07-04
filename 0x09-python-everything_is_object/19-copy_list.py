f7abfb6087247b4cf67f2e36cbec5f2ebf18fdba
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-04 17:52:46 +0000

    19 fix new

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module contains the copy_list function"""
 
-def copy_list(tab):
-    return tab[:]
+def copy_list(my_list):
+    """
+    Returns a copy of a list
+
+    Args:
+        my_list (list): The input list to be copied
+    """
+    return my_list[:]
