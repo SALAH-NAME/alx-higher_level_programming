58d5513493b0a26d9bad13a8ca1efa457ebac9bc
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-11 22:28:40 +0000

    fix 7.1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
