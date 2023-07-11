7d9748a50e3114ebb4cf313a8459d33ed89d2353
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-11 22:32:33 +0000

    fix 7.2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This global comment for a file to test checker.
+"""
 import sys
 from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -6,11 +9,16 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
+"""
+Check if the file exists.
+"""
 if path.exists(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
-
+"""
+Add command line arguments to the list.
+"""
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
