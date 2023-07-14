99b91e48ffee7af20de7400fc0067cf58396aaee
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-14 16:43:39 +0000

    to 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,8 @@ class Rectangle(Base):
     def area(self):
         """Returns the area value of the Rectangle instance"""
         return self.width * self.height
+
+    def display(self):
+        """Prints in stdout the Rectangle instance with the character #"""
+        for i in range(self.height):
+            print("#" * self.width)
