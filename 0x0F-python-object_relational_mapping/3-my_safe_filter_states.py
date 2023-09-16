c5f1b55badc24b9a3ba7729fb31a1544b3d6aceb
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-16 20:11:51 +0000

    fix_3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ import sys
 if __name__ = "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.arv[2], db=sys.argv[3])
     c = db.cursor()
-    st = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    st = "SELECT * FROM states ORDER BY id ASC"
     c.execute(st, (sys.argv[4],))
     rows = c.fetchall()
     for row in rows:
