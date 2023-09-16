ddf6a61bdefa65d847aacf9ec6d587b3bb4b5b29
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-16 21:04:21 +0000

    fix_4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     c.execute("SELECT cities.id, cities.name, states.name FROM cities \
                 JOIN states ON cities.state_id = states.id \
                 ORDER BY cities.id ASC")
-    rows = cur.fetchall()
+    rows = c.fetchall()
     for row in rows:
         print(row)
     c.close()
