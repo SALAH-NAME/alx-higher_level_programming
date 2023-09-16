c1d05a9fc4399e8c109350ba3cb33f1dcc78997f
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-16 15:18:56 +0000

    2to4

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that lists all states from the db where name matches the arg"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    st = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    c = execute(st, (sys.argv[4],))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database"""
+
+import MySQLdb
+import sys
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    st = "SELECT cities.id, cities.name, \
+            states.name FROM cities JOIN states ON \
+            cities.state_id = states.id ORDER BY cities.id ASC"
+    c.execute(st)
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
