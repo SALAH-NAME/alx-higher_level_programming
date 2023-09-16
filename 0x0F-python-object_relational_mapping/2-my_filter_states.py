918171f95f73eb17cd2cd4d4e105359933c7fa36
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-16 15:26:58 +0000

    fix_2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Script that lists all states from the db where name matches the arg
-Usage: ./2-my_filter_states.py <mysql username> \
-                               <mysql password> \
-                               <database name> \
-                               <state name searched>
+"""
+    Script that lists all states from the db where name matches the arg
 """
 
 import MySQLdb
@@ -12,8 +9,8 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    st = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    c = execute(st, (sys.argv[4],))
+    st = "SELECT * FROM states WHERE name = {} ORDER BY id ASC"
+    c = execute(st.format(sys.argv[4]))
     rows = c.fetchall()
     for row in rows:
         print(row)
