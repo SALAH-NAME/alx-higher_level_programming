d4d1e1637af77054420ea3d39a4918b189f32f29
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-16 20:17:57 +0000

    fix_3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the db where name matches the arg
-This version is safe from SQL injections.
-"""
+"""Script that takes in an arg and displays all values in the states table"""
+
 
 import MySQLdb
 import sys
 
-if __name__ = "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.arv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    st = "SELECT * FROM states ORDER BY id ASC"
-    c.execute(st)
+    c.execute("SELECT * FROM `states`")
     rows = c.fetchall()
     for row in rows:
         if row[1] == sys.argv[4]:
