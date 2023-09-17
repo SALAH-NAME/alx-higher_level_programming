1fcfb79d5826255102509f7bd6e0e13ca3e9dedc
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-17 09:21:39 +0000

    old

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -13,6 +13,7 @@ class State(Base):
     State class that inherits from Base.
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship(
+            "City", backref="state", cascade="all, delete-orphan")
