02af2b0213a16375fccac4370b5b8f5176edc9f9
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-17 07:56:01 +0000

    fix_14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,6 +7,13 @@ from model_state import Base, State
 
 
 class City(Base):
+    """
+    Represents a city for a MySQL database.
+    Attributes:
+        id (sqlalchemy.Integer): The unique identifier of the city.
+        name (sqlalchemy.String): The name of the city.
+        state_id (sqlalchemy.Integer): The identifier of the state.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
