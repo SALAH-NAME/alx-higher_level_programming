9cfc66bbc450f8ba548fd3804bb054b6a823a117
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-17 09:20:16 +0000

    fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,6 +11,6 @@ class City(Base):
     Represents a city for a MySQL database.
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
