00668ae0eb8a17813ed027656896252a2bc3db99
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-17 07:45:45 +0000

    14_fetch

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define the City class which inherits from Base"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
