06a5453179940176f5431c50547c15bc3a71ceaf
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-28 22:16:34 +0000

    000

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,20 +4,17 @@
 
 def find_peak(list_of_integers):
     """ Find a peak in a list of Int-s"""
-    if not list_of_integers:
+    if len(list_of_integers) == 0:
         return None
 
-    length = len(list_of_integers)
-    if length == 1:
-        return list_of_integers[0]
-    elif length == 2:
-        return max(list_of_integers)
+    low = 0
+    high = len(list_of_integers) - 1
 
-    mid = length // 2
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and
-    list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
